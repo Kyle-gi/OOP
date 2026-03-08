670d6a17bb996f2e998b330b99bd14c43ec189ea
Your Name <Kyle-gi@users.noreply.github.com.>
2026-03-08 20:12:54 +0100

    changes

---
+++ src/Main.java
@@ -0,0 +1,3 @@
+public class Main{
+    static int[] numero = {10, 1, 2, 3};
+}
---
+++ src/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    public static void main(String[] args){
+        Main main = new Main();
+
+        for (int i = 0; i < Main.numero.length; i++) {
+            System.out.println(Main.numero[i]);
+        }
+    }
+}
