7f49a96b9e3828776d5c25ccd11b51132b3e3f9e
Your Name <Kyle-gi@users.noreply.github.com.>
2026-03-14 13:47:23 +0100

    changes

--- src/Exercise.java
+++ src/Exercise.java
@@ -9,8 +9,20 @@ public class Exercise {
         return sum == number;
     }
 
-    public static boolean esAbundante(){
+    public static boolean esAbundante(int number){
+        int sum = 0;
 
+        for(int i = 1; i < number; i++){
+            if (number % i == 0){
+                sum += i;
+            }
+        }
+        if(sum > number){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public static boolean esDefectuoso(){
