161034d061208953dbbbbb85fc044739b3503c57
Kyle-gi <hyrokai.oliver@gmail.com>
2026-03-16 10:53:02 +0100

    changes

--- src/Exercise.java
+++ src/Exercise.java
@@ -20,12 +20,23 @@ public class Exercise {
         return sum > number;
     }
 
-    public static boolean esDefectuoso(){
+    public static boolean esDefectuoso(int number){
+        int sum = 0;
 
+        for (int i = 1; i < number; i++){
+            if(number % i == 0){
+                sum += i;
+            }
+        }
+        return sum < number;
     }
 
-    public static boolean esOdioso(){
+    public static boolean esOdioso(int number){
+        int sum = 0;
 
+        for (int i = 1; i < number; i++){
+            if(number )
+        }
     }
 
     public static boolean esPrimo(){
