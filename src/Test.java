b11e8e42c5b08ca834cbc359ea0c344fa822b09f
Your Name <Kyle-gi@users.noreply.github.com.>
2026-03-17 19:16:59 +0100

    changes

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,53 @@
-public class Test {
+import java.util.Scanner;
 
+
+public class Test {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("===SELECT ONE OPTION===");
+        int option = 0;
+
+        switch (option){
+            case 1:
+                int number = 0;
+                System.out.print("Insert a number you want: ");
+                scanner.nextInt();
+                OperacionesVarias.esPerfecto(number);
+            case 2:
+                int abundant = 0;
+                System.out.print("Insert a number you want: ");
+                scanner.nextInt();
+                OperacionesVarias.esAbundante(abundant);
+            case 3:
+                int defectuoso = 0;
+                System.out.print("Insert a number you want: ");
+                scanner.nextInt();
+                OperacionesVarias.esDefectuoso(defectuoso);
+            case 4:
+                int odioso = 0;
+                System.out.print("Insert a number you want: ");
+                scanner.nextInt();
+                OperacionesVarias.esOdioso(odioso);
+            case 5:
+                int primo = 0;
+                System.out.print("Insert a number you want: ");
+                scanner.nextInt();
+                OperacionesVarias.esPrimo(primo);
+            case 6:
+                int amigo = 0;
+                int amigo1 = 0;
+                System.out.print("Insert two numbers: ");
+                scanner.nextInt();
+                OperacionesVarias.esAmigo(amigo, amigo1);
+            case 7:
+                int enemigo = 0;
+                int enemigo1 = 0;
+                System.out.print("Insert two numbers: ");
+                scanner.nextInt();
+                OperacionesVarias.esEnemigo(enemigo, enemigo1);
+            case 8:
+                OperacionesVarias.dibujarPiramide();
+        }
+    }
 }
--- src/fileWriter/Constantes.java
+++
@@ -1,6 +0,0 @@
-package fileWriter;
-
-public class Constantes {
-    public static final String[] NOMBRES = {"Luna", "Kiarash", "Dani", "Alvaro"};
-
-}
