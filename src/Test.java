180810a2af2808e81779fab79dce6cc0b808c1ff
Kyle-gi <hyrokai.oliver@gmail.com>
2026-03-18 18:02:04 +0100

    changes

--- src/Test.java
+++ src/Test.java
@@ -5,49 +5,63 @@ public class Test {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("===SELECT ONE OPTION===");
-        int option = 0;
+        System.out.println("===SELECT ONE OPTION===");
+        System.out.println("1. Es perfecto");
+        System.out.println("2. Es abundante");
+        System.out.println("3. Es defectuoso");
+        System.out.println("4. Es odioso");
+        System.out.println("5. Es primo");
+        System.out.println("6. Es amigo");
+        System.out.println("7. Es enemigo");
+        System.out.println("8. Dibujar Piramide");
+
+        int option = scanner.nextInt();
 
         switch (option){
             case 1:
-                int number = 0;
                 System.out.print("Insert a number you want: ");
-                scanner.nextInt();
-                OperacionesVarias.esPerfecto(number);
+                int number = scanner.nextInt();
+                System.out.println("Result: " + OperacionesVarias.esPerfecto(number));
+                break;
             case 2:
-                int abundant = 0;
+
                 System.out.print("Insert a number you want: ");
-                scanner.nextInt();
-                OperacionesVarias.esAbundante(abundant);
+                int abundant = scanner.nextInt();
+                System.out.println("Result: " + OperacionesVarias.esAbundante(abundant));
+                break;
             case 3:
-                int defectuoso = 0;
+
                 System.out.print("Insert a number you want: ");
-                scanner.nextInt();
-                OperacionesVarias.esDefectuoso(defectuoso);
+                int defectuoso = scanner.nextInt();
+                System.out.println("Result: " + OperacionesVarias.esDefectuoso(defectuoso));
+                break;
             case 4:
-                int odioso = 0;
+
                 System.out.print("Insert a number you want: ");
-                scanner.nextInt();
-                OperacionesVarias.esOdioso(odioso);
+                int odioso = scanner.nextInt();
+                System.out.println("Result: " + OperacionesVarias.esOdioso(odioso));
+                break;
             case 5:
-                int primo = 0;
+
                 System.out.print("Insert a number you want: ");
-                scanner.nextInt();
+                int primo = scanner.nextInt();
                 OperacionesVarias.esPrimo(primo);
+                break;
             case 6:
-                int amigo = 0;
-                int amigo1 = 0;
                 System.out.print("Insert two numbers: ");
-                scanner.nextInt();
+                int amigo = scanner.nextInt();
+                int amigo1 = scanner.nextInt();
                 OperacionesVarias.esAmigo(amigo, amigo1);
+                break;
             case 7:
-                int enemigo = 0;
-                int enemigo1 = 0;
                 System.out.print("Insert two numbers: ");
-                scanner.nextInt();
+                int enemigo = scanner.nextInt();
+                int enemigo1 = scanner.nextInt();
                 OperacionesVarias.esEnemigo(enemigo, enemigo1);
+                break;
             case 8:
                 OperacionesVarias.dibujarPiramide();
+                break;
         }
     }
 }
