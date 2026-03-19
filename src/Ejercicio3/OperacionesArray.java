8bd318c87ceb6cce169f1137be9ce9298503a652
Your Name <Kyle-gi@users.noreply.github.com.>
2026-03-19 23:12:46 +0100

    changes

--- src/Ejercicio3/OperacionesArray.java
+++ src/Ejercicio3/OperacionesArray.java
@@ -1,4 +1,14 @@
 package Ejercicio3;
 
+import java.util.Scanner;
+
 public class OperacionesArray {
+    Scanner sc = new Scanner(System.in);
+    public static void loadArray(){
+        int [] insert = new int[10];
+
+        for(int i = 0; i < insert.length; i++){
+            System.out.print("");
+        }
+    }
 }
