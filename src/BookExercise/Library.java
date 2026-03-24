b5e6b433518ae69954605118ac74701dcf43ea7a
Your Name <Kyle-gi@users.noreply.github.com.>
2026-03-24 18:04:12 +0100

    changes

--- src/BookExercise/Library.java
+++ src/BookExercise/Library.java
@@ -9,6 +9,13 @@ public class Library {
     }
 
     public void addBook(Book b){
-
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = b;
+            }
+            System.out.print("The book " + b.getTitle() + " has been added to the library.");
+            return;
+        }
+        System.out.println("Library is already full cannot add: " + b.getTitle());
     }
 }
